b4e3a0475a2c9bf2299fd7b85a542534d1b0d4dc
Josue Ponce <josue.ponce.ferrera@hp.com>
2021-04-07 19:01:42 +0200

    Finished chapter 1 - Python fundamentals

---
+++ 02_basic_datatypes/2_strings/02_08_occurrence.py
@@ -0,0 +1,11 @@
+'''
+Write a script that takes a string of words and a letter from the user.
+Find the index of first occurrence of the letter in the string. For example:
+
+String input: hello world
+Letter input: o
+Result: 4
+
+'''
+string = "hello world"
+print(string.index("o"))
