b16a23addca24fec8f2b80056c47cec52913ebc1
Josue7912 <81706691+Josue7912@users.noreply.github.com>
2021-05-13 19:01:42 +0200

    Finished Chapter 16

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,8 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+def cities(*args):
+    for location in args:
+        print(f"* {location}")
+
+cities("London", "Paris", "Madrid", "Budapest")
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,8 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+def objectlist(**kwargs):
+    for k, v in kwargs.items():
+        print(f"{v} is a {k}")
+
+objectlist(clothes="t-shirt", car="Cupra", brand="Zara")
